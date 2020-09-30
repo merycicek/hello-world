e5d2ad89e90533eebfb11cf053032dca6fdf8470
None <merycicek@bastion-fuchicorp-com.us-central1-a.c.geometric-sled-282503.internal>
2020-09-30 23:45:27 +0000

    adding new changes

--- deployments/terraform/variable.tf
+++ deployments/terraform/variable.tf
@@ -1,25 +1,19 @@
-variable "deployment_image" {
-  default = "docker.merycicek.com/hello-world-app-dev-feature:fe57059"
+variable  "deployment_image" {
+    default = "docker.fuchicorp.com/hello-world-app-dev-feature:4b7aacb"
 }
 variable "deployment_environment" {
     default = "dev"
 }
-
 variable "deployment_endpoint" {
-  type = "map"
-  default = {
-    test  = "test.hello"
-    dev  = "dev.hello"
-    qa   = "qa.hello"
-    prod = "hello"
-    stage = "stage.hello"
+    type = "map"
+     default = {
+        test  = "test.hello"
+        dev  = "dev.hello"
+        qa   = "qa.hello"
+        prod = "hello"
+        stage = "stage.hello"
   }
 }
-
 variable "google_domain_name" {
-  default = "merycicek.com"
-}
-
-output "application_endpoint" {
-  value = "${lookup(var.deployment_endpoint, "${var.deployment_environment}")}.${var.google_domain_name}"
+    default = "fuchicorp.com"
 }
